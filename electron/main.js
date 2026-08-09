535a3322d0bad7f7ee7a5ea6fc3ee48e1d697cbb
Mostafa Maghwry <mostafa_magdy@live.com>
2026-08-09 13:20:30 +0300

    feat: integrate Electron with Vite and restructure project

--- electron/main.js
+++ electron/main.js
@@ -1,25 +1,24 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1400,
         height: 900,
-        minWidth: 1200,
-        minHeight: 700,
-        autoHideMenuBar: true,
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
+            preload: path.join(__dirname, "preload.js"),
+            contextIsolation: true
         }
     });
 
-    win.loadFile(path.join(__dirname, '../renderer/index.html'));
+    if (isDev) {
+        win.loadURL("http://localhost:5173");
+        win.webContents.openDevTools();
+    } else {
+        win.loadFile(path.join(__dirname, "../dist/index.html"));
+    }
 }
 
 app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin')
-        app.quit();
-});
---
+++ src/renderer/js/main.js
@@ -0,0 +1,1 @@
+console.log("ThreeMarket POS Started");
